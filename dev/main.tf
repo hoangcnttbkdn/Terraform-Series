1950a926970e55ef2572e14d10c5b1a9f1f152f7
Hoàng Huy <hoangcnttbkdn@gmail.com>
2023-04-20 12:39:37 +0700

    create ssh key and add it to ecs instance

--- dev/main.tf
+++ dev/main.tf
@@ -9,19 +9,50 @@ module "vpc" {
   create_nat_gateway = false
 }
 
-resource "aws_key_pair" "hoangbvh_key" {
-  key_name   = "hoangbvh_key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDxblHyALvIRvj5CzYQO3TG088y38ySd0CFex+c9JhD7dk9iGxbfZ/z2dGX/6KkH+P2AXlukOI0ze+8uIGARUcLCE/UCA3O59J1fJBOl1ujn6AesDRxqdKC00tQJMvQ5O27wXLZpv8DnwL14k9kqskV+dYv4YP/NAtTz7Spw3zbQTROIdwI71Qa5SiMPwVhklQjKWjSEA8+RpDTtfklz7Qw5idIa+e10yzoKkjaBs2pUjhFFO1mNr/OEjI6uY+BvnbZcDi5Mr9RjBS5t9VWcHjaAnwaVl6O68yjhkrE1ZkDyC3jTQE32X+h0b9qbTLuQe/E6XdeBBnV22Ui9cNUuY4dSkOYPqnkSAOPHqKo748xS4bnqDF/hFUrwap+eTntmoT39gL7DEdz/jHD8U1+5hBvrWyXPojPasiapni9KL7VxLZWExeTGh4STLgbbrINzVzfAbqi+6c81xqWayFRO8IE+XYHOSHsVY5FTISCjxAZohgyCPWEulvebWSpk8+7HSE= huyhoang@nitro5"
+# resource "aws_key_pair" "hoang_key" {
+#   key_name   = "hoang_key"
+#   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDxblHyALvIRvj5CzYQO3TG088y38ySd0CFex+c9JhD7dk9iGxbfZ/z2dGX/6KkH+P2AXlukOI0ze+8uIGARUcLCE/UCA3O59J1fJBOl1ujn6AesDRxqdKC00tQJMvQ5O27wXLZpv8DnwL14k9kqskV+dYv4YP/NAtTz7Spw3zbQTROIdwI71Qa5SiMPwVhklQjKWjSEA8+RpDTtfklz7Qw5idIa+e10yzoKkjaBs2pUjhFFO1mNr/OEjI6uY+BvnbZcDi5Mr9RjBS5t9VWcHjaAnwaVl6O68yjhkrE1ZkDyC3jTQE32X+h0b9qbTLuQe/E6XdeBBnV22Ui9cNUuY4dSkOYPqnkSAOPHqKo748xS4bnqDF/hFUrwap+eTntmoT39gL7DEdz/jHD8U1+5hBvrWyXPojPasiapni9KL7VxLZWExeTGh4STLgbbrINzVzfAbqi+6c81xqWayFRO8IE+XYHOSHsVY5FTISCjxAZohgyCPWEulvebWSpk8+7HSE= huyhoang@nitro5"
+# }
+
+# resource "aws_key_pair" "my_key_pair" {
+#   key_name   = "my-key-pair"
+#   public_key = file("/home/huyhoang/.ssh/id_rsa.pub")
+# }
+
+# resource "aws_instance" "my_instance" {
+#   ami           = "ami-052c9af0c988f8bbd"
+#   instance_type = "t2.micro"
+#   subnet_id     = element(module.vpc.public_subnet_ids, 0)
+#   key_name      = aws_key_pair.hoang_key.key_name
+#   user_data     = <<-EOF
+#     #!/bin/bash
+#     echo "Hello, World" > index.html
+#   EOF
+
+# }
+
+
+resource "tls_private_key" "pk" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "kp" {
+  key_name   = "myKey"       # Create "myKey" to AWS!!
+  public_key = tls_private_key.pk.public_key_openssh
+
+  provisioner "local-exec" { # Create "myKey.pem" to your computer!!
+    command = "echo '${tls_private_key.pk.private_key_pem}' > ./myKey.pem"
+  }
 }
 
 resource "aws_instance" "my_instance" {
   ami           = "ami-052c9af0c988f8bbd"
   instance_type = "t2.micro"
   subnet_id     = element(module.vpc.public_subnet_ids, 0)
-  key_name      = aws_key_pair.hoangbvh_key.key_name
+  key_name      = aws_key_pair.kp.key_name
   user_data     = <<-EOF
     #!/bin/bash
     echo "Hello, World" > index.html
   EOF
-  
 }
